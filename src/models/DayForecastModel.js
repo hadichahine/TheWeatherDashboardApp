f8f39afb2880b8e65d3d08feee92ad40e57e316c
Hadi Chahine <hadichahine232@hotmail.com>
2020-01-14 00:51:02 +0200

    Move array computation into getDailyForecase directly.

--- src/models/DayForecastModel.js
+++ src/models/DayForecastModel.js
@@ -1,47 +1,28 @@
 import createObservable from './Observable.js'
+import DailyForecast from '../DailyForecast.js'
 
 function retrieveDayNameByDate(date){
 	return Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(date)
 }
 
-function tabulateHourlyWeatherRecordsIntoDailyForecastMatrix(hourlyRecords){
-	let currentIndex = -1
-	let currentDay
-	let array = []
-	hourlyRecords
-	.forEach((hourlyRecord) => {
-		if(hourlyRecord.date.getDate() !== currentDay) {
-			currentDay = hourlyRecord.date.getDate()
-			currentIndex++
-			array[currentIndex] = []
-		}
-		array[currentIndex].push(hourlyRecord)
-	})
-	return array
-}
-
-import DailyForecast from '../DailyForecast.js'
-
 export default {
 	__proto__: createObservable(),
-	forecastsForEachComingDay: [],
-	updateDailyWeatherForecast(dailyWeatherForecastArray){
-		this.forecastsForEachComingDay = 
-		dailyWeatherForecastArray
-		.map((hourlyForecastForOneDay) => {
-			return new DailyForecast(hourlyForecastForOneDay)
-		}).map((dailyForecast) => {
-			return {
-				day: retrieveDayNameByDate(dailyForecast.date()),
-				highTemperature: dailyForecast.highTemperature(),
-				lowTemperature: dailyForecast.lowTemperature(),
-				condition: dailyForecast.condition()
-		    }
-		})
+	hourWeatherPerDayForecastMatrix: [],
+	updateDailyWeatherForecast(hourWeatherPerDayForecastMatrix){
+		this.hourWeatherPerDayForecastMatrix = hourWeatherPerDayForecastMatrix;
 		this.inform();
 	},
 	getDailyForecaseForFirstN(count){
-		return this.forecastsForEachComingDay.slice(0, count);
+		return this.hourWeatherPerDayForecastMatrix
+				   .map((hourlyForecastForOneDay) => {
+						let dailyForecast = new DailyForecast(hourlyForecastForOneDay)
+						return {
+							day: retrieveDayNameByDate(dailyForecast.date()),
+							highTemperature: dailyForecast.highTemperature(),
+							lowTemperature: dailyForecast.lowTemperature(),
+							condition: dailyForecast.condition()
+		    			}
+					}).slice(0, count);
 	},
 	getDailyForecast(){
 		return this.forecastsForEachComingDay;
